a749daca361452e7a26fae6b56965461841c9565
xamdp <64017084+xamdp@users.noreply.github.com>
2025-12-08 23:18:32 +0900

    ch6: initial version from book
    Add memcpy and memset as for memory operations.
    Add strcpy and strcmp as for string operations.
    
    Why the initial version?
    
    I had this idea that to truly understand the contents that this book
    offers. I need to force myself to re-implement it.

--- kernel.c
+++ kernel.c
@@ -4,12 +4,6 @@ typedef uint32_t size_t;
 
 extern char __bss[], __bss_end[], __stack_top[];
 
-void *memset(void *buf, char c, size_t n) {
-	uint8_t *p = (uint8_t *) buf;
-	while (n--)
-		*p++ = c;
-	return buf;
-}
 
 void kernel_main(void) {
 	memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
